7679e547dc3d17e44c1280e7df9eefe4ba76ea4b
wuchunlei <chunleiw@wicresoft.com>
2021-03-23 20:16:17 +0800

    feat:rAsset/swap page

--- app/pages/rAsset/swap/index.tsx
+++ app/pages/rAsset/swap/index.tsx
@@ -1,16 +1,96 @@
-import React, { useEffect } from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import Content from '@shared/components/content';
 import Title from '@shared/components/cardTitle';
 import Back from '@shared/components/backIcon';
-import TypeInput from '@shared/components/input/typeInput'
+import Input from '@shared/components/input/addressInput';
+import TypeInput from '@shared/components/input/typeInput';
+import {rSymbol,Symbol} from '@keyring/defaults';
+import Button from '@shared/components/button/button'
 import './index.scss'
+import rETH_svg from '@images/rETH.svg';
+import selected_rETH from '@images/selected_rETH.svg'; 
+import rFIS_svg from '@images/rFIS.svg';
+import selected_rFIS from '@images/selected_rFIS.svg'; 
+import rDOT from '@images/rDOT.svg';
+import selected_rDOT from '@images/selected_rDOT.svg'; 
+const datas=[{
+  icon:rETH_svg,
+  selectedIcon:selected_rETH,
+  title:"Atom",
+  amount:"23.289",
+  type:Symbol.Atom
+},{
+  icon:rFIS_svg,
+  selectedIcon:selected_rFIS,
+  title:"rFIS",
+  amount:"23.289",
+  type:Symbol.Fis
+},{
+  icon:rDOT,
+  selectedIcon:selected_rDOT,
+  title:"rDOT",
+  amount:"23.289",
+  type:Symbol.Dot
+}]
 export default function Index(props:any){ 
  
+  const [fromAoumt,setFormAmount]=useState();
+  const [fromType,setFormType]=useState(datas[0]);
+  const [toAoumt,setToAmount]=useState();
+  const [toType,setToType]=useState(datas[0]);
+  const [address,setAddress]=useState();
   return  <Content className="stafi_rasset_swap">
       <Back />
       <Title label="rBridge Swap"/>
       <div>
-        <TypeInput />
+        <div className="row">
+          <div className="label">
+              <label>From</label>
+              <label className="balance">rFIS balance 233.424</label>
+          </div>
+          <div>
+            <TypeInput placeholder="0.0" value={fromAoumt} onChange={(value:any)=>{
+              setFormAmount(value)
+            }} 
+            selectDataSource={datas} 
+            type={fromType}
+            onSelectChange={(e:any)=>{ 
+              setFormType(e);
+            }}/> 
+          </div>
+        </div>
+       
+        <div className="row">
+          <div className="label">
+              <label>To</label> 
+          </div>
+          <div>
+            <TypeInput placeholder="0.0" value={toAoumt} onChange={(value:any)=>{
+              setFormAmount(value)
+            }} 
+            selectDataSource={datas} 
+            type={toType}
+            onSelectChange={(e:any)=>{ 
+              setToType(e);
+            }}/> 
+          </div>
+        </div>
+
+        <div className="row last"> 
+          <div>
+            <Input placeholder="To Ethereum Address" value={address} onChange={(e:any)=>{
+              setAddress(e.target.value)
+            }}/> 
+          </div>
+        </div>
+        <div className="fee">
+            Estimate Fee: 12.23 FIS
+        </div>
+        <div className="btns">
+        <Button  onClick={()=>{
+             
+         }}>Swap</Button>
+        </div>
       </div>
   </Content>
 }
--- app/shared/components/input/typeInput.tsx
+++ app/shared/components/input/typeInput.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import {Input} from 'antd'; 
-import selected_rDOT_svg from '@images/selected_rDOT.svg';
-import bottom_arrow from '@images/bottom_arrow.svg';
+import React, { useState } from 'react';
+import {Input,Popover} from 'antd';  
+import black_close from '@images/black_close.svg';
 import leftArrowSvg from '@images/left_arrow.svg';
+
+import rETH_svg from '@images/rETH.svg';
 import './index.scss';
 
 type Props={
     placeholder?:string, 
     unit?:string,
     value?:string | number ,
-    onChange?:Function
+    onChange?:Function,
+    onSelectChange?:Function,
+    selectDataSource?:any[],
+    type?:any
 }
 export default function Index(props:Props){
+
+    const [showSelect,setShowSelect]=useState(false)
     return <Input 
     className="amount_input type_input" 
     onChange={(e)=>{  
@@ -20,7 +26,56 @@ export default function Index(props:Props){
     }}
     value={props.value}
     placeholder={props.placeholder} 
-    suffix={<div><img src={selected_rDOT_svg} />rFIS <img src={leftArrowSvg} /></div>}/>
+    suffix={<Popover visible={showSelect} placement="bottomRight" overlayClassName="stafi_type_input_select" title={<SelectTitle onClose={()=>{
+      setShowSelect(false)
+    }}/>} content={<Select 
+      selectDataSource={props.selectDataSource}
+      fromType={props.type}
+      onSelectChange={(e:any)=>{ 
+        props.onSelectChange && props.onSelectChange(e);
+        setShowSelect(false)
+      }}/>} trigger="click">
+      {props.type?<div onClick={()=>{
+      setShowSelect(true);
+    }}><img src={props.type.selectedIcon} />{props.type.title} <img src={leftArrowSvg} /></div>:<div></div>}</Popover>}/>
+}
+
+type SelectTitleProps={
+  onClose?:Function
+}
+function SelectTitle(props:SelectTitleProps){
+  return  <div className='title'>
+  <label>Select a token</label>
+  <img src={black_close} onClick={()=>{
+    props.onClose && props.onClose()
+  }}/>
+</div>
+}
+
+type SelectProps={
+  onSelectChange?:Function,
+  fromType:any,
+  selectDataSource?:any[]
+}
+function Select(props:SelectProps){
+  return <div className="content">
+
+      {props.selectDataSource && props.selectDataSource.map(item=>{
+        return <div className="item"  onClick={()=>{
+                  props.onSelectChange && props.onSelectChange(item)
+                }}>
+                <div className="title">
+                  <div>
+                    <img src={item.icon} />
+                  </div>
+                  {item.title}
+                </div>
+                <label className="amount">
+                    {item.amount}
+                </label>
+          </div>
+      })} 
+    </div> 
 }
 
 {/* <Popover placement="bottomRight" title={text} content={content} trigger="click">
