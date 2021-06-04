75df1569c1f373d0727bb1206b984105ad9adcb0
zhangtong <289649077@qq.com>
2021-06-04 15:21:51 +0800

    Update description of Validator Process

--- app/pages/rETH/validator/home/validatorContent.tsx
+++ app/pages/rETH/validator/home/validatorContent.tsx
@@ -13,19 +13,19 @@ export default function Index(props:any){
         1.Deposit
         </div>
         <div className="describe">
-            Deposit 8 ETH to register as a delegated validator on StaFi, StaFi will match 24 ETH to your node so that it can meet the validator conditions of  ETH2.0. 
+        Deposit 8 ETH to register as a delegated validator on StaFi, StaFi Staking Contract will match 24 ETH to your node so that it can meet the validator requirements of ETH2.0.
         </div>
         <div className="sub_title">
         2.Stake
         </div>
         <div className="describe"> 
-            Once your node reaches 32ETH,  you can deploy a pool contract to deposit 32ETH to the Deposit contract on ETH1.0, and then wait for validating progress on ETH2.0.
+        Once 32ETH is matched to your node, you can deploy a pool contract to deposit 32ETH to the Deposit contract on ETH1.0, and then wait for validation progress on ETH2.0.
         </div>
         <div className="sub_title">
         3.Status
         </div>
         <div className="describe"> 
-            There is nothing we can do in this step, once you stake, we should wait for the queen, check the status on the dashboard
+        There is nothing we can do in this step, once you stake, we should wait for the queen, check the status on the dashboard.
         </div>
         <div className="btns">
             <Button  onClick={()=>{
