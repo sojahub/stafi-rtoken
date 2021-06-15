609e3d31794474370b29314b2add17d0b867961a
shanzhiping <danzhiping@sina.com>
2021-06-15 16:23:19 +0800

    fix:reward type

--- app/pages/rDOT/staker/reward.tsx
+++ app/pages/rDOT/staker/reward.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import RewardContent from '@components/reward/index'
 import DataItem from '@components/reward/dataItem'
 export default function Index(){
-    return <RewardContent type="ATOM">
+    return <RewardContent type="DOT">
          <DataItem era={1} tokenAmount={2.234323} ratio={32.321232} redeemableToken={2.342345} reward={23.432345} />
          <DataItem era={2} tokenAmount={2.234323} ratio={32.321232} redeemableToken={2.342345} reward={23.432345} />
          <DataItem era={2} tokenAmount={2.234323} ratio={32.321232} redeemableToken={2.342345} reward={23.432345} />
