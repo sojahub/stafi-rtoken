d6f5ece48969e61e3091e98d82065b97f83b2b38
zhangtong <289649077@qq.com>
2021-06-17 08:43:32 +0800

    Update some contract addresses for testnet

--- app/config/index.ts
+++ app/config/index.ts
@@ -41,35 +41,42 @@ export default {
     if(!isdev()){
       return '0x9559aaa82d9649c7a7b220e7c461d2e74c9a3593';
     }else{
-      return '0x680ab46340aa2189515b49fd35ac8a5bd66e78de';
+      return '0x0ed54e1b7b3be1c02d91b4fa8bf5655f3fbe08b4';
     }  
   },
   stafiUserDepositAddress:()=> {
     if(!isdev()){
       return '0x430cf6dd3e289adae63b50ff661d6bba2dbb3f28';
     }else{
-      return '0x310b80843c56591bd3c403f877ab665f68530cef';
+      return '0x6b3d7a220b96f3be9ff48e6be36a7e16f46b1393';
     }   
   },
   stafiNodeDepositAddress:() => { 
     if(!isdev()){
       return '0x50db2ce93c8b1f6771c985b6b840b587349496a0';
     }else{
-      return '0x0313d2b2e9ef926012881a7e33482a957bed265c';
+      return '0xf072c7e6e36639870c3986196237a97fcccb0331';
+    }   
+  },
+  stafiNodeManagerAddress:() => { 
+    if(!isdev()){
+      return '0x4fd35afa32310eaa1354768be6ad2c5c6a62d572';
+    }else{
+      return '0x68b749894c5484687916d57616b5214cf9fc63cb';
     }   
   },
   stafiStakingPoolManagerAddress:()=> {
     if(!isdev()){
       return '0x1c9890c9cb9925a8651c10b5f557d744bafbed5a';
     }else{
-      return '0xa84ec99b9c9d16f769d9909a2466923f4dddd282';
+      return '0x3f1ea0333e9e1caba4ff3f4d44c0808a2eaa8468';
     }  
   },
   stafiStakingPoolQueueAddress:()=> { 
     if(!isdev()){
       return '0xc59ea6cebb8089a0330800f50946610977c4fc96';
     }else{
-      return '0xc8985dd9da3c9d35eaac6b29b5f6b7ecc3f765ca';
+      return '0x40a0f8f23dbc635b8e54c8b785c62269cad8ebf8';
     }  
   },
   rBridgeApp: ()=>{ 
