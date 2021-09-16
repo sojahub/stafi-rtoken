f2095b0ef4a50ca8a0438d55ce1f09735c93ff3f
joe <joewu590@gmail.com>
2021-09-16 17:54:16 +0800

    split node_modules bundle

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -67,6 +67,19 @@ const options = {
     // },
     splitChunks: {
       chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+  
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
     },
   },
 };
