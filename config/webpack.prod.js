cd8ff9c59395a33f11a11edfcac2b96a4a28af41
joe <joewu590@gmail.com>
2021-09-26 15:37:37 +0800

    Update webpack.prod.js

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -47,19 +47,24 @@ const options = {
     ],
     splitChunks: {
       chunks: 'all',
+      minSize: 1000000,
+      maxSize: 5000000,
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          // name: "vendors",
-          // chunks: "initial",
+          priority: -10,
           name(module) {
             // get the name. E.g. node_modules/packageName/not/this/part.js
             // or node_modules/packageName
             const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-  
             // npm package names are URL-safe, but some servers don't like @ symbols
             return `npm.${packageName.replace('@', '')}`;
           },
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
         },
       },
     },
