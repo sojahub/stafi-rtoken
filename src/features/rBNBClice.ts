0841f2d0db40df80e59410bdbf363cb472810870
Tore19 <289649077@qq.com>
2021-11-10 15:02:51 +0800

    Calculate 7-day annualization based on the era rate

--- src/features/rBNBClice.ts
+++ src/features/rBNBClice.ts
@@ -843,13 +843,12 @@ export const rTokenLedger = (): AppThunk => async (dispatch, getState) => {
 const handleStakerApr =
   (currentRate?: any, lastRate?: any): AppThunk =>
   async (dispatch, getState) => {
-    dispatch(setStakerApr('20.6%'));
-    // if (currentRate && lastRate) {
-    //   const apr = NumberUtil.handleEthRoundToFixed(((currentRate - lastRate) / 1000000000000 / 7) * 365.25 * 100) + '%';
-    //   dispatch(setStakerApr(apr));
-    // } else {
-    //   dispatch(setStakerApr('9.7%'));
-    // }
+    if (currentRate && lastRate) {
+      const apr = NumberUtil.handleEthRoundToFixed(((currentRate - lastRate) / 1000000000000 / 7) * 365.25 * 100) + '%';
+      dispatch(setStakerApr(apr));
+    } else {
+      dispatch(setStakerApr('9.7%'));
+    }
   };
 
 export const checkAddress = (address: string) => {
