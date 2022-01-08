a397223c86a6868420697a9be812c827f85136f3
Tore19 <289649077@qq.com>
2022-01-08 14:58:10 +0800

    Revert

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,6 @@ import { clusterApiUrl } from '@solana/web3.js';
 import { rSymbol, Symbol } from 'src/keyring/defaults';
 
 export const isdev = () => {
-  return false;
   let host = window.location.host;
   var local = /192\.168\./.test(host) || /127\.0\./.test(host) || /localhost/.test(host);
   let demo = /test/.test(host);
