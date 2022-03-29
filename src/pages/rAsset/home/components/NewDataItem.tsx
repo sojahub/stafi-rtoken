8c79e3ee3df6b6f27a5557a6ca6a651ca631c431
wkq91 <joewu590@gmail.com>
2022-03-29 18:24:51 +0800

    refine onClick in asset item

--- src/pages/rAsset/home/components/NewDataItem.tsx
+++ src/pages/rAsset/home/components/NewDataItem.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { useLastEraReward } from 'src/hooks/useEraReward';
 import { Tooltip } from 'antd';
 import { useHistory } from 'react-router';
+import { e } from 'mathjs';
 
 interface NewDataItemProps {
   rTokenName: string;
@@ -42,15 +43,14 @@ export const NewDataItem = (props: NewDataItemProps) => {
 
   return (
     <>
-      <TokenItemContainer>
+      <TokenItemContainer
+        onClick={() => {
+          history.push(`/${props.rTokenName}/staker/info?platform=${props.source}`);
+        }}>
         <HContainer
           style={{
             paddingLeft: '47px',
             width: '174px',
-            cursor: 'pointer',
-          }}
-          onClick={() => {
-            history.push(`/${props.rTokenName}/staker/info?platform=${props.source}`);
           }}>
           <img src={props.icon} width='26px' height='26px' alt='icon' />
           <TokenTitle>{props.rTokenName}</TokenTitle>
@@ -59,10 +59,6 @@ export const NewDataItem = (props: NewDataItemProps) => {
         <TableContent
           style={{
             width: '137px',
-            cursor: 'pointer',
-          }}
-          onClick={() => {
-            history.push(`/${props.rTokenName}/staker/info?platform=${props.source}`);
           }}>
           <HContainer>
             <div
@@ -94,10 +90,6 @@ export const NewDataItem = (props: NewDataItemProps) => {
           style={{
             width: '106px',
             opacity: props.rTokenAmount === '--' || Number(props.myStaked) === 0 ? 0.4 : 1,
-            cursor: 'pointer',
-          }}
-          onClick={() => {
-            history.push(`/${props.rTokenName}/staker/info?platform=${props.source}`);
           }}>
           {props.rTokenAmount}
         </TableContent>
@@ -105,10 +97,6 @@ export const NewDataItem = (props: NewDataItemProps) => {
         <TableContent
           style={{
             width: '96px',
-            cursor: 'pointer',
-          }}
-          onClick={() => {
-            history.push(`/${props.rTokenName}/staker/info?platform=${props.source}`);
           }}>
           {props.apy}
         </TableContent>
@@ -119,7 +107,8 @@ export const NewDataItem = (props: NewDataItemProps) => {
               opacity: 1,
               cursor: 'pointer',
             }}
-            onClick={() => {
+            onClick={(e) => {
+              e.stopPropagation();
               setTradeModalVisible(true);
             }}>
             <div>Trade</div>
@@ -128,7 +117,10 @@ export const NewDataItem = (props: NewDataItemProps) => {
             style={{
               marginLeft: '5px',
             }}
-            onClick={props.onSwapClick}>
+            onClick={(e) => {
+              e.stopPropagation();
+              props.onSwapClick();
+            }}>
             Bridge
           </StakeButton>
         </HContainer>
@@ -161,6 +153,7 @@ const TokenItemContainer = styled.div`
   margin-bottom: 15px;
   display: flex;
   align-items: center;
+  cursor: pointer;
 `;
 
 const TokenTitle = styled.div`
