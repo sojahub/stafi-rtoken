e4cdc9a91a864b5591b8574ddfb3f6164892157f
Tore19 <289649077@qq.com>
2022-05-05 10:18:18 +0800

    Update SOL RPC

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,6 @@ import { clusterApiUrl } from '@solana/web3.js';
 import { rSymbol, Symbol } from 'src/keyring/defaults';
 
 export const isdev = () => {
-  return false;
   let host = window.location.host;
   var local = /192\.168\./.test(host) || /127\.0\./.test(host) || /localhost/.test(host);
   let demo = /test/.test(host);
@@ -150,7 +149,7 @@ const config = {
   solRpcApi: () => {
     if (!isdev()) {
       // return clusterApiUrl('mainnet-beta');
-      return 'https://solana-mainnet.phantom.tech';
+      return 'https://solana.public-rpc.com';
     } else {
       // return clusterApiUrl('devnet');
 
