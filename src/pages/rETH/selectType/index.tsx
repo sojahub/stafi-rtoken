a3ce06bf535e599d6a5ecfd151e31b70ad40983d
joe <joewu590@gmail.com>
2022-10-27 15:41:27 +0800

    change rETH validator url

--- src/pages/rETH/selectType/index.tsx
+++ src/pages/rETH/selectType/index.tsx
@@ -30,7 +30,7 @@ export default function Index(props: any) {
         if (e === 'Staker') {
           props.history.push('/rETH/staker');
         } else {
-          window.open('https://reth-validator.stafi.io/reth/token-stake?checkNewUser=true');
+          window.open('https://reth-validator.stafi.io/validator/reth/token-stake?checkNewUser=true');
         }
       }}
     />
--- src/pages/rETH/systemData.ts
+++ src/pages/rETH/systemData.ts
@@ -19,7 +19,7 @@ export const rDOT_data = [
   },
   {
     text: 'Validator',
-    url: 'https://reth-validator.stafi.io/reth/token-stake?checkNewUser=true',
+    url: 'https://reth-validator.stafi.io/validator/reth/token-stake?checkNewUser=true',
     child: [
       {
         text: '-Deposit',
